b2499a72e1188f509f07c3c568dee54e20398182
Daria Ovchinnikova <daria_ovch@mail.ru>
2020-10-08 18:20:51 +0200

    js logic

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,54 @@
-const radioButtons = Array.from(document.querySelectorAll('.window__input'));
-const numbers = Array.from(document.querySelector('.window__input_type_select'));
-const numberOfLicenses = numbers.selectedIndex.value;
+//Константа для отображения итоговой суммы
 const sum = document.querySelector('.window__sum');
+const selectedPlan = document.querySelector('.window__plan');
 
-const countTotal = () => {
-    radioButtons.forEach((item) => {
-        if(item.checked) {
-            return item.value
-        }
+//Константа для окна выбора количества лицензий
+const select = document.querySelector('.window__input_type_select');
 
-        return item.value * numberOfLicenses;
-    })
+//Получаем значение выбранной радиокнопки
+function getRadioValue() {
+    const radio = document.getElementsByName('license-type');
+     for (let i=0; i<radio.length; i++) {
+         if(radio[i].checked) {
+             selectedPlan.textContent = `Selected plan: #${i+1}`;
+             return radio[i].value;
+         }
+     }
+ }
+
+ //Получаем значение выбранного количества лицензий
+select.onChange = () => {
+    return select.value;
 }
 
-sum.textContent = countTotal;
+//Считаем итоговую сумму
+function countTotal() {
+        let n = select.onChange();
+        let m = getRadioValue();
+
+        let total = m * n;
+
+         return total;
+    
+}
+
+//Слушатель события 'change' для формы
+document.forms.licenses.addEventListener('change', () => {
+    let total = countTotal();
+    sum.textContent = `$${total}US`;
+})
+
+
+
+const submitButton = document.querySelector('.window__submit-button');
+
+function handleSubmitForm() {
+    document.forms.licenses.classList.add('window__form_submitted');
+    document.querySelector('.window__payment').classList.remove('window__payment_inactive');
+}
+
+document.forms.licenses.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    document.querySelector('.window__sum-to-pay').textContent = `$${countTotal()}US`
+    handleSubmitForm();
+})
